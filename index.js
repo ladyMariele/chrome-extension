73bf7bf05d0890f2c9c798371a1a1a881703a379
Mariele <102813740+ladyMariele@users.noreply.github.com>
2023-03-15 23:59:50 +0800

    add tab button

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn
 
 function render(leads) {
     let listItems = ""
